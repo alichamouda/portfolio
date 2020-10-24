97debc52c0b7ae6dfa8b7a64dd6a618b7f270cd9
alichamouda <ali.c.hamouda@gmail.com>
2020-10-24 18:00:29 +0100

    Last Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import {FlexModule} from "@angular/flex-layout/typings/flex";
+import { SocialMediaLinksComponent } from './social-media-links/social-media-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainContainerComponent,
+    SocialMediaLinksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
